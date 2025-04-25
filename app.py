a686cd2ff131075a2b2c6827fc893badd2b25591
LionRouge1 <matchoudiavlessi@gmail.com>
2025-04-25 14:34:28 +0000

    Update the menu to display the new options

--- app.py
+++ app.py
@@ -25,12 +25,15 @@ class App():
 
   def app_options(self):
     print(Fore.CYAN + "\nPlease choose from the following options:\n" + Style.RESET_ALL)
-    print("1. Search for businesses")
-    print("2. Load a file")
-    print("3. Split data")
-    print("4. Categorize the data")
-    print("5. Remove duplicates businesses")
-    print("6. Exit\n")
+    print("1. Search Search businesses without website")
+    print("2. Search businesses with website")
+    print("3. Load a file")
+    print("4. Split data")
+    print("5. Categorize the data")
+    print("6. Remove duplicates businesses")
+    print("7. Remove all businesses")
+    print("8. Exit\n")
+    print(Fore.YELLOW + "Note: Please ensure the file is in the correct format." + Style.RESET_ALL)
 
   def run(self):
     self.welcome()
@@ -46,27 +49,40 @@ class App():
               continue
             self.scraper.search_for_businesses(query)
             self.app_options()
+
           case 2:
+            query = input("Enter your search query: ")
+            if not query.strip():
+              print(Fore.RED + "Query cannot be empty. Please try again." + Style.RESET_ALL)
+              continue
+            self.scraper.search_for_businesses(query, website=True)
+            self.app_options()
+          case 3:
             file = input("Enter the filename: ").strip()
             if not Path(file).exists() and not file.endswith(".csv") and not file:
               print(Fore.RED + "Sorry we can't foud the file" + Style.RESET_ALL)
               continue
             spliter.laod_data(file)
             self.app_options()
-          case 3:
+          case 4:
             spliter.split_data()
             self.app_options()
 
-          case 4:
+          case 5:
             spliter.sort_by_category()
             self.app_options()
 
-          case 5:
+          case 6:
             spliter.write_unique_businesses()
             print(Fore.GREEN + "Unique businesses have been written to unique_businesses.csv" + Style.RESET_ALL)
             self.app_options()
 
-          case 6:
+          case 7:
+            spliter.empty_businesses()
+            print(Fore.GREEN + "All businesses have been removed." + Style.RESET_ALL)
+            self.app_options()
+
+          case 8:
             print(Fore.GREEN + "Exiting..." + Style.RESET_ALL)
             break
           case _:
